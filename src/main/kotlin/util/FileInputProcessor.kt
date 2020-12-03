48307fcd0ab9165a46b286912adbc7eebaa93364
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2020-12-03 14:44:09 +0000

    Added read2dCharArray function.

--- src/main/kotlin/util/FileInputProcessor.kt
+++ src/main/kotlin/util/FileInputProcessor.kt
@@ -2,10 +2,14 @@ package util
 
 import java.io.File
 
-fun readLines(file : File) : List<String> {
+fun readLines(file: File): List<String> {
     return file.readLines()
 }
 
-fun readIntegers(file : File) : List<Int> {
+fun readIntegers(file: File): List<Int> {
     return readLines(file).map { it.toInt() }
+}
+
+fun read2dCharArray(file: File): Array<CharArray> {
+    return readLines(file).map { it.toCharArray() }.toTypedArray()
 }
