b0250600f4cab82d8f59a49cc332634f3c0bdbc2
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2020-12-04 11:26:01 +0000

    Upgraded JUnit Jupiter to 5.7.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ dependencies {
     implementation(kotlin("reflect"))
 
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 }
 
 tasks.test {
