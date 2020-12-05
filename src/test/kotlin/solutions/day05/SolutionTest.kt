27fbe34960d47073799e322577e685ca635351de
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2020-12-05 18:32:15 +0000

    Day05: Added puzzle tests.

---
+++ src/test/kotlin/solutions/day05/SolutionTest.kt
@@ -0,0 +1,8 @@
+package solutions.day05
+
+import GenericSolutionTest
+
+internal class SolutionTest : GenericSolutionTest(Solution()) {
+    override val expectedPart1Result = "904"
+    override val expectedPart2Result = "669"
+}
