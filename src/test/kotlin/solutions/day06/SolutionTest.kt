7e4f848f4aae892be39a9ed8e2acb9fe7b9a26dd
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2020-12-06 07:49:15 +0000

    Day06: Added tests for both puzzles.

---
+++ src/test/kotlin/solutions/day06/SolutionTest.kt
@@ -0,0 +1,8 @@
+package solutions.day06
+
+import GenericSolutionTest
+
+internal class SolutionTest : GenericSolutionTest(Solution()) {
+    override val expectedPart1Result = "6542"
+    override val expectedPart2Result = "3299"
+}
