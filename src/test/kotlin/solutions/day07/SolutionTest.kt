cf4b8314a181b6bb75dc81aeca078182649865d2
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2020-12-07 14:13:45 +0000

    Day07: Added solution test.

---
+++ src/test/kotlin/solutions/day07/SolutionTest.kt
@@ -0,0 +1,8 @@
+package solutions.day07
+
+import GenericSolutionTest
+
+internal class SolutionTest : GenericSolutionTest(Solution()) {
+    override val expectedPart1Result = "142"
+    override val expectedPart2Result = "10219"
+}
