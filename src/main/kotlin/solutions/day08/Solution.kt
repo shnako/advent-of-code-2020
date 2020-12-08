ea4fc6ad82223204bf3de33e99a9d3596aa6c48b
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2020-12-08 09:54:34 +0000

    Day08: Added puzzle text and input
    as I'll be on a flight and I might work on it

---
+++ src/main/kotlin/solutions/day08/Solution.kt
@@ -0,0 +1,14 @@
+package solutions.day08
+
+import solutions.GenericSolution
+import java.io.File
+
+class Solution : GenericSolution {
+    override fun runPart1(inputFile: File): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun runPart2(inputFile: File): String {
+        TODO("Not yet implemented")
+    }
+}
