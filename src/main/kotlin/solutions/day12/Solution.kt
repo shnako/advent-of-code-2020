54b32a056182c2e58d611774732379c4d69cc1e1
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2020-12-12 08:34:40 +0000

    Day12: Added solution to part 2.

--- src/main/kotlin/solutions/day12/Solution.kt
+++ src/main/kotlin/solutions/day12/Solution.kt
@@ -20,6 +20,11 @@ private data class ShipPosition(
     var bearing: Char
 )
 
+private data class WaypointPosition(
+    var x: Int,
+    var y: Int
+)
+
 class Solution : GenericSolution {
     private fun turn(shipPosition: ShipPosition, action: Char, value: Int) {
         for (turn in 0 until value / 90) {
@@ -49,10 +54,10 @@ class Solution : GenericSolution {
     // North & East positive, South & West negative
     private fun navigate(shipPosition: ShipPosition, action: Char, value: Int) {
         when (action) {
-            NORTH -> shipPosition.x += value
-            EAST -> shipPosition.y += value
-            SOUTH -> shipPosition.x -= value
-            WEST -> shipPosition.y -= value
+            NORTH -> shipPosition.y += value
+            EAST -> shipPosition.x += value
+            SOUTH -> shipPosition.y -= value
+            WEST -> shipPosition.x -= value
             LEFT -> turn(shipPosition, action, value)
             RIGHT -> turn(shipPosition, action, value)
             FORWARD -> navigate(shipPosition, shipPosition.bearing, value)
@@ -76,7 +81,64 @@ class Solution : GenericSolution {
         return (abs(shipPosition.x) + abs(shipPosition.y)).toString()
     }
 
+    private fun turnWaypoint(waypointPosition: WaypointPosition, action: Char, value: Int) {
+        for (turn in 0 until value / 90) {
+            when (action) {
+                LEFT -> {
+                    val y = waypointPosition.x
+                    waypointPosition.x = -waypointPosition.y
+                    waypointPosition.y = y
+                }
+                RIGHT -> {
+                    val y = -waypointPosition.x
+                    waypointPosition.x = waypointPosition.y
+                    waypointPosition.y = y
+                }
+            }
+        }
+    }
+
+    private fun navigateToWaypoint(shipPosition: ShipPosition, waypointPosition: WaypointPosition, value: Int) {
+        shipPosition.x = shipPosition.x + value * waypointPosition.x
+        shipPosition.y = shipPosition.y + value * waypointPosition.y
+    }
+
+    private fun navigateByWaypoint(
+        shipPosition: ShipPosition,
+        waypointPosition: WaypointPosition,
+        action: Char,
+        value: Int
+    ) {
+        when (action) {
+            NORTH -> waypointPosition.y += value
+            EAST -> waypointPosition.x += value
+            SOUTH -> waypointPosition.y -= value
+            WEST -> waypointPosition.x -= value
+            LEFT -> turnWaypoint(waypointPosition, action, value)
+            RIGHT -> turnWaypoint(waypointPosition, action, value)
+            FORWARD -> navigateToWaypoint(shipPosition, waypointPosition, value)
+        }
+    }
+
+    private fun navigateByWaypoint(
+        shipPosition: ShipPosition,
+        waypointPosition: WaypointPosition,
+        instructions: List<String>
+    ) {
+        instructions.forEach {
+            val action = it[0]
+            val value = it.substring(1).toInt()
+            navigateByWaypoint(shipPosition, waypointPosition, action, value)
+        }
+    }
+
     override fun runPart2(inputFile: File): String {
-        TODO("Not yet implemented")
+        val instructions = readLines(inputFile)
+
+        val shipPosition = ShipPosition(0, 0, EAST)
+        val waypointPosition = WaypointPosition(10, 1)
+        navigateByWaypoint(shipPosition, waypointPosition, instructions)
+
+        return (abs(shipPosition.x) + abs(shipPosition.y)).toString()
     }
 }
---
+++ src/test/kotlin/solutions/day12/SolutionTest.kt
@@ -0,0 +1,8 @@
+package solutions.day12
+
+import GenericSolutionTest
+
+internal class SolutionTest : GenericSolutionTest(Solution()) {
+    override val expectedPart1Result = "1032"
+    override val expectedPart2Result = "156735"
+}
