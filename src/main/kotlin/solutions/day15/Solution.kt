c17ec1bb98bc5683670c84a6712acb8cf18c4838
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2020-12-15 07:05:16 +0000

    Day15: Added solution to part 2.

--- src/main/kotlin/solutions/day15/Solution.kt
+++ src/main/kotlin/solutions/day15/Solution.kt
@@ -13,7 +13,7 @@ class Solution : GenericSolution {
             .toMutableList()
     }
 
-    private fun playTurn(spokenNumbers: MutableList<Long>) {
+    private fun playTurnNaive(spokenNumbers: MutableList<Long>) {
         var lastNumberSpoken = -1L
         var previousOccurrence = -1L
 
@@ -36,12 +36,39 @@ class Solution : GenericSolution {
         }
     }
 
-    private fun play(turns: Long, numbers: MutableList<Long>): Long {
-        for (turn in numbers.size until turns) {
-            playTurn(numbers)
-            if (turn % 1000 == 0L) {
-                println(turn)
+    private fun playTurnOptimal(
+        previousLastSpokenNumber: Long,
+        spokenNumbers: MutableList<Long>,
+        lastIndexMap: HashMap<Long, Long>
+    ): Long {
+        val lastSpokenNumber = spokenNumbers.last()
+
+        when {
+            lastSpokenNumber == previousLastSpokenNumber -> {
+                spokenNumbers.add(1)
             }
+            lastIndexMap.containsKey(lastSpokenNumber) -> {
+                spokenNumbers.add(spokenNumbers.size - lastIndexMap[lastSpokenNumber]!!)
+            }
+            else -> {
+                spokenNumbers.add(0)
+            }
+        }
+
+        lastIndexMap[previousLastSpokenNumber] = (spokenNumbers.size - 2).toLong()
+
+        return lastSpokenNumber
+    }
+
+    private fun play(turns: Long, numbers: MutableList<Long>): Long {
+        val lastIndexMap = HashMap<Long, Long>()
+        for (i in 0..numbers.size - 2) {
+            lastIndexMap[numbers[i]] = (i + 1).toLong()
+        }
+
+        var previousLastSpokenNumber = numbers[numbers.size - 2]
+        for (turn in numbers.size until turns) {
+            previousLastSpokenNumber = playTurnOptimal(previousLastSpokenNumber, numbers, lastIndexMap)
         }
 
         return numbers.last()
@@ -54,6 +81,8 @@ class Solution : GenericSolution {
     }
 
     override fun runPart2(inputFile: File): String {
-        TODO()
+        val numbers = parseNumber(inputFile)
+        val result = play(30000000, numbers)
+        return result.toString()
     }
 }
---
+++ src/test/kotlin/solutions/day15/SolutionTest.kt
@@ -0,0 +1,8 @@
+package solutions.day15
+
+import GenericSolutionTest
+
+internal class SolutionTest : GenericSolutionTest(Solution()) {
+    override val expectedPart1Result = "1522"
+    override val expectedPart2Result = "18234"
+}
