f993ac083fb88ce0e3353692e278408dc42973bd
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2020-12-17 10:01:11 +0000

    Day17: Added 4D neighbours.

--- src/main/kotlin/util/NeighbourUtil.kt
+++ src/main/kotlin/util/NeighbourUtil.kt
@@ -75,3 +75,113 @@ fun are3DCoordinatesValid(x: Int, y: Int, z: Int, grid3D: Array<Array<BooleanArr
 }
 
 // endregion
+
+
+// region 4D
+
+val neighbours4D = arrayOf(
+    arrayOf(-1, -1, -1, -1),
+    arrayOf(-1, -1, -1, +0),
+    arrayOf(-1, -1, -1, +1),
+    arrayOf(-1, -1, +0, -1),
+    arrayOf(-1, -1, +0, +0),
+    arrayOf(-1, -1, +0, +1),
+    arrayOf(-1, -1, +1, -1),
+    arrayOf(-1, -1, +1, +0),
+    arrayOf(-1, -1, +1, +1),
+
+    arrayOf(-1, +0, -1, -1),
+    arrayOf(-1, +0, -1, +0),
+    arrayOf(-1, +0, -1, +1),
+    arrayOf(-1, +0, +0, -1),
+    arrayOf(-1, +0, +0, +0),
+    arrayOf(-1, +0, +0, +1),
+    arrayOf(-1, +0, +1, -1),
+    arrayOf(-1, +0, +1, +0),
+    arrayOf(-1, +0, +1, +1),
+
+    arrayOf(-1, +1, -1, -1),
+    arrayOf(-1, +1, -1, +0),
+    arrayOf(-1, +1, -1, +1),
+    arrayOf(-1, +1, +0, -1),
+    arrayOf(-1, +1, +0, +0),
+    arrayOf(-1, +1, +0, +1),
+    arrayOf(-1, +1, +1, -1),
+    arrayOf(-1, +1, +1, +0),
+    arrayOf(-1, +1, +1, +1),
+
+
+    arrayOf(+0, -1, -1, -1),
+    arrayOf(+0, -1, -1, +0),
+    arrayOf(+0, -1, -1, +1),
+    arrayOf(+0, -1, +0, -1),
+    arrayOf(+0, -1, +0, +0),
+    arrayOf(+0, -1, +0, +1),
+    arrayOf(+0, -1, +1, -1),
+    arrayOf(+0, -1, +1, +0),
+    arrayOf(+0, -1, +1, +1),
+
+    arrayOf(+0, +0, -1, -1),
+    arrayOf(+0, +0, -1, +0),
+    arrayOf(+0, +0, -1, +1),
+    arrayOf(+0, +0, +0, -1),
+    arrayOf(+0, +0, +0, +1),
+    arrayOf(+0, +0, +1, -1),
+    arrayOf(+0, +0, +1, +0),
+    arrayOf(+0, +0, +1, +1),
+
+    arrayOf(+0, +1, -1, -1),
+    arrayOf(+0, +1, -1, +0),
+    arrayOf(+0, +1, -1, +1),
+    arrayOf(+0, +1, +0, -1),
+    arrayOf(+0, +1, +0, +0),
+    arrayOf(+0, +1, +0, +1),
+    arrayOf(+0, +1, +1, -1),
+    arrayOf(+0, +1, +1, +0),
+    arrayOf(+0, +1, +1, +1),
+
+
+    arrayOf(+1, -1, -1, -1),
+    arrayOf(+1, -1, -1, +0),
+    arrayOf(+1, -1, -1, +1),
+    arrayOf(+1, -1, +0, -1),
+    arrayOf(+1, -1, +0, +0),
+    arrayOf(+1, -1, +0, +1),
+    arrayOf(+1, -1, +1, -1),
+    arrayOf(+1, -1, +1, +0),
+    arrayOf(+1, -1, +1, +1),
+
+    arrayOf(+1, +0, -1, -1),
+    arrayOf(+1, +0, -1, +0),
+    arrayOf(+1, +0, -1, +1),
+    arrayOf(+1, +0, +0, -1),
+    arrayOf(+1, +0, +0, +0),
+    arrayOf(+1, +0, +0, +1),
+    arrayOf(+1, +0, +1, -1),
+    arrayOf(+1, +0, +1, +0),
+    arrayOf(+1, +0, +1, +1),
+
+    arrayOf(+1, +1, -1, -1),
+    arrayOf(+1, +1, -1, +0),
+    arrayOf(+1, +1, -1, +1),
+    arrayOf(+1, +1, +0, -1),
+    arrayOf(+1, +1, +0, +0),
+    arrayOf(+1, +1, +0, +1),
+    arrayOf(+1, +1, +1, -1),
+    arrayOf(+1, +1, +1, +0),
+    arrayOf(+1, +1, +1, +1),
+)
+
+fun are4DCoordinatesValid(x: Int, y: Int, z: Int, w: Int, grid4D: Array<Array<Array<BooleanArray>>>): Boolean {
+    if (x < 0 || y < 0 || z < 0 || w < 0) {
+        return false
+    }
+
+    if (x >= grid4D.size || y >= grid4D[x].size || z >= grid4D[x][y].size || w >= grid4D[x][y][z].size) {
+        return false
+    }
+
+    return true
+}
+
+// endregion
