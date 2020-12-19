4e9de6ccd3b482efd72a098583ac4b5060f0c30a
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2020-12-19 10:28:51 +0000

    Day19: Added solution for part 2.

--- src/main/kotlin/solutions/day19/Solution.kt
+++ src/main/kotlin/solutions/day19/Solution.kt
@@ -12,6 +12,12 @@ data class Rule(
     val character: Char?
 )
 
+/**
+ * This solution recursively builds a Regex string based on the rules
+ * and then counts how many messages match it.
+ *
+ * For part 2 I simply stop recursing while building the Regex string after a number of steps.
+ */
 class Solution : GenericSolution {
     private fun parseRule(ruleText: String): Rule {
         val numberAndRuleTextComponents = ruleText.split(":")
@@ -46,7 +52,7 @@ class Solution : GenericSolution {
         )
     }
 
-    private fun parseRules(inputText: String): Map<Int, Rule> {
+    private fun parseRules(inputText: String): MutableMap<Int, Rule> {
         return inputText
             .split("\n\n")[0]
             .split("\n")
@@ -54,6 +60,7 @@ class Solution : GenericSolution {
             .map { parseRule(it) }
             .map { it.number to it }
             .toMap()
+            .toMutableMap()
     }
 
     private fun parseMessages(inputText: String): List<String> {
@@ -63,42 +70,61 @@ class Solution : GenericSolution {
             .filter { StringUtils.isNotBlank(it) }
     }
 
-    private fun buildRegexBasedOnRules(rule: Rule, rules: Map<Int, Rule>): String {
+    private fun buildRegexBasedOnRules(depth: Int, rule: Rule, rules: Map<Int, Rule>): String {
         if (rule.character != null) {
             return rule.character.toString()
         }
 
         val firstPart = rule.subRules!!
-            .joinToString(separator = "") { buildRegexBasedOnRules(rules[it]!!, rules) }
+            .joinToString(separator = "") { buildRegexBasedOnRules(depth + 1, rules[it]!!, rules) }
+
+        // Only go down 16 levels to avoid recursing infinitely.
+        // This is sufficient for my input.
+        if (depth > 16) {
+            return firstPart
+        }
 
         return if (rule.orSubRules == null) {
             firstPart
         } else {
             val secondPart = rule.orSubRules
-                .joinToString(separator = "") { buildRegexBasedOnRules(rules[it]!!, rules) }
+                .joinToString(separator = "") { buildRegexBasedOnRules(depth + 1, rules[it]!!, rules) }
             "($firstPart|$secondPart)"
         }
     }
 
-    override fun runPart1(inputFile: File): String {
-        val inputText = readFileAsString(inputFile)
-        val rules = parseRules(inputText)
-        val messages = parseMessages(inputText)
-
-        val regexStr = buildRegexBasedOnRules(rules[0]!!, rules)
+    private fun countMessagesMatchingRules(messages: List<String>, rules: Map<Int, Rule>): Int {
+        val regexStr = buildRegexBasedOnRules(0, rules[0]!!, rules)
         println("Regex string based on rules: $regexStr")
         val regex = Regex(regexStr)
 
         return messages
             .filter { regex.matches(it) }
             .count()
-            .toString()
+    }
+
+    override fun runPart1(inputFile: File): String {
+        val inputText = readFileAsString(inputFile)
+        val messages = parseMessages(inputText)
+        val rules = parseRules(inputText)
+
+        return countMessagesMatchingRules(messages, rules).toString()
+    }
+
+    private fun replaceRules(rules: MutableMap<Int, Rule>) {
+        val newRule8 = "8: 42 | 42 8"
+        val newRule11 = "11: 42 31 | 42 11 31"
+
+        rules[8] = parseRule(newRule8)
+        rules[11] = parseRule(newRule11)
     }
 
     override fun runPart2(inputFile: File): String {
         val inputText = readFileAsString(inputFile)
-        val rules = parseRules(inputText)
         val messages = parseMessages(inputText)
-        TODO("Not yet implemented")
+        val rules = parseRules(inputText)
+        replaceRules(rules)
+
+        return countMessagesMatchingRules(messages, rules).toString()
     }
 }
---
+++ src/test/kotlin/solutions/day19/SolutionTest.kt
@@ -0,0 +1,8 @@
+package solutions.day19
+
+import GenericSolutionTest
+
+internal class SolutionTest : GenericSolutionTest(Solution()) {
+    override val expectedPart1Result = "173"
+    override val expectedPart2Result = "367"
+}
