cba01759238ed02733570b9acf81728f81182682
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2020-12-20 09:24:22 +0000

    Extracted BinaryUtil.kt for Day 14.

--- src/main/kotlin/solutions/day14/Solution.kt
+++ src/main/kotlin/solutions/day14/Solution.kt
@@ -1,34 +1,13 @@
 package solutions.day14
 
 import solutions.GenericSolution
+import util.binaryToLong
+import util.longToBinary
 import util.readLines
 import java.io.File
 
 @Suppress("DuplicatedCode") // Keeping some small duplicates for clarity.
 class Solution : GenericSolution {
-    private fun binaryToLong(binary: List<Int>): Long {
-        var multiplier = 1L
-        var number = 0L
-        for (i in binary.count() - 1 downTo 0) {
-            if (binary[i] == 1) {
-                number += multiplier
-            }
-            multiplier *= 2
-        }
-        return number
-    }
-
-    private fun longToBinary(number: Long, bits: Int): MutableList<Int> {
-        val numberInBinary = number.toString(2).toCharArray().map { it.toInt() - 48 }
-
-        val correctlySizedNumberInBinary = mutableListOf<Int>()
-        for (i in numberInBinary.size until bits) {
-            correctlySizedNumberInBinary.add(0)
-        }
-        correctlySizedNumberInBinary.addAll(numberInBinary)
-
-        return correctlySizedNumberInBinary.toMutableList()
-    }
 
     private fun applyMask(number: Long, mask: String): Long {
         val bits = mask.length
