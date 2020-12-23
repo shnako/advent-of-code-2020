f49e394f9f741769adb673997b91860dcdf841dd
Vlad Schnakovszki <vlad.schnakovszki@skyscanner.net>
2020-12-23 08:20:39 +0000

    Day23: Added solution to part 1.

---
+++ src/main/kotlin/solutions/day23/Solution.kt
@@ -0,0 +1,87 @@
+package solutions.day23
+
+import solutions.GenericSolution
+import util.readFileAsString
+import java.io.File
+import java.util.*
+
+data class Game(
+    val cups: LinkedList<Int>,
+    val min: Int,
+    val max: Int,
+)
+
+class Solution : GenericSolution {
+    private fun readCups(inputFile: File): Game {
+        val cups = readFileAsString(inputFile)
+            .toCharArray()
+            .dropLast(1)
+            .map { it.toInt() - 48 }
+
+        return Game(
+            LinkedList(cups),
+            cups.minOrNull()!!,
+            cups.maxOrNull()!!,
+        )
+    }
+
+    private fun findValidLabel(label: Int, game: Game): Int {
+        for (i in label - 1 downTo game.min) {
+            if (game.cups.contains(i)) {
+                return i
+            }
+        }
+
+        return -1
+    }
+
+    private fun playMove(game: Game) {
+        val current = game.cups.removeFirst()
+        val nextCup1 = game.cups.removeFirst()
+        val nextCup2 = game.cups.removeFirst()
+        val nextCup3 = game.cups.removeFirst()
+
+
+        var destinationCupLabel = findValidLabel(current, game)
+
+        if (destinationCupLabel == -1) {
+            destinationCupLabel = findValidLabel(game.max + 1, game)
+        }
+
+        val insertIndex = game.cups.indexOf(destinationCupLabel) + 1
+        game.cups.add(insertIndex, nextCup3)
+        game.cups.add(insertIndex, nextCup2)
+        game.cups.add(insertIndex, nextCup1)
+
+        game.cups.addLast(current)
+    }
+
+    private fun getResult(game: Game): Long {
+        while (game.cups.peekFirst() != 1) {
+            game.cups.addLast(game.cups.removeFirst())
+        }
+
+        game.cups.removeFirst()
+
+        var multiplier = 1L
+        var result = 0L
+        while (game.cups.isNotEmpty()) {
+            result += multiplier * game.cups.removeLast()
+            multiplier *= 10
+        }
+
+        return result
+    }
+
+    override fun runPart1(inputFile: File): String {
+        val game = readCups(inputFile)
+        for (i in 1..100) {
+            playMove(game)
+        }
+        return getResult(game).toString()
+    }
+
+    override fun runPart2(inputFile: File): String {
+        TODO("Not yet implemented")
+    }
+}
